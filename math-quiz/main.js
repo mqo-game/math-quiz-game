000ac570cb03cb732b30bc725089ebf2e533b179
McJoe21 <39857764+McJoe21@users.noreply.github.com>
2020-03-21 21:18:20 +0000

    Update 0.3-beta
    - Added a new about page for information
    - More question types based on differently

--- math-quiz/main.js
+++ math-quiz/main.js
@@ -4,7 +4,7 @@ function submit() {
     var email = document.getElementById("email").value;
 
     if (name !== "" || email !== "") {
-    base = diff * 10;
+    base = diff * 9;
     localStorage.setItem("name", name)
     localStorage.setItem("base", base)
 
@@ -13,7 +13,8 @@ function submit() {
       alert("Please enter your name.")
   }
  }
-function index() {document. getElementById("ver").innerHTML = "0.2.1-beta"}
+function index() {
+  document.getElementById("ver").innerHTML = "0.3-beta"}
 function main() {
     $q = 1; 
     document.getElementById("q").innerHTML = $q
@@ -21,8 +22,7 @@ function main() {
     $base = localStorage.getItem("base")
     if ($base !== 0) {
        new_sum();
-    } else {document.location.assign('index.html')}
-    
+    } else {document.location.replace('index.html')}
 }
 
 function type_num(num) {
@@ -34,10 +34,34 @@ function type_num(num) {
 }
 
 function new_sum() {
-    $num_1 = Math.floor(Math.random() * $base)
-    $num_2 = Math.floor(Math.random() * $base)
+  /* random base gen */
+    $num_1 = Math.floor(Math.random() * $base + 1)
+    $num_2 = Math.floor(Math.random() * $base + 1)
+    
+    /* check differently */
+    $diff = $base/9
+    if ($diff == 1) {
+      $type = 1
+    } else if ($diff == 2 ||$diff == 3) {
+      $type = Math.floor(Math.random()*1 + 1)
+    } 
+    else if ($diff == 4 ||$diff == 5) {
+      $type = Math.floor(Math.random()*2 + 1)
+    }
+    
+    /* generate sums */
+    if ($type == 1) {
     $answer = $num_1 + $num_2
-    $str = $num_1 + " + " + $num_2 + " = ?"
+    $sum = "+"
+    } else if ($type == 2) {
+     $answer = $num_1 - $num_2
+     $sum = "-"
+    } else if ($type == 3) {
+      $answer = $sum_1 * $num_2
+      $sum = "x"
+    }
+    
+    $str = $num_1 + " " + $sum + " " + $num_2 + " = ?"
     document.getElementById("sum").innerHTML = $str
 }    
 function check() {
@@ -50,6 +74,6 @@ function check() {
         new_sum();
     } else {
         alert("Incorrect answer.")
-        window.location.assign('index.html')
+        window.location.replace('index.html')
         }   
     }
