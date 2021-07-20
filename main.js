a6c4706c9617b215d0d8daa9fb5c87f327804aae
McJoe21 <39857764+McJoe21@users.noreply.github.com>
2021-07-20 22:53:26 +0100

    0.4.1-dev5
    - New locked Endless mode

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-//on page load
-const date = new Date()
+	//set variables 
+const date = new Date();
 
 var u = {
- "name": "demo",
+ "name": "Player123",
  "pts": 0,
  "plays": 0,
  "ver": "041",
  "data":{"pts":{},"plays":{}}
 };
-
-//set const variables
-const stats = "%1 games played with %2 points in total!"
-const page = document.getElementsByClassName('page')
+const stats = "%1 games played with %2 points in total!";
+const page = document.getElementsByClassName('page');
+const form = document.querySelector("form");
 
 //user data checks
 u.date = date.toDateString();
@@ -20,8 +19,10 @@ if (!localStorage["mqo-user"]) {
 } else {
  u = JSON.parse(localStorage["mqo-user"]);
  u.date = date.toDateString();
- if(u.ver = "040"){u.data = {"pts":{},"plays":{}};u.ver = "041"}
+ if(u.ver == "040"){u.data = {"pts":{},"plays":{}};u.ver = "041";}
 }
+var endless_req = u.pts > 49 && u.plays > 9;
+if(endless_req){form.target[5].innerHTML = 'Target: Endless';form.target[5].disabled = false}
 
 if ('serviceWorker' in navigator && 'PushManager' in window) {
  navigator.serviceWorker.register('./app.js', {
@@ -31,94 +32,99 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 
 //start of functions
 function end_game(pts,pass) {
- u.plays++
- u.pts = Number(u.pts) + pts
- u.pts2 = pts
+ home_stats()
+ u.plays++;
+ u.pts = Number(u.pts) + pts;
+ u.pts2 = pts;
+ u.data.plays[$diff]++;
+ u.data.pts[$diff] = u.pts;
  localStorage["mqo-user"] = JSON.stringify(u);
- pages('game-over')
+ pages('game-over');
+ $a.value = "";
 
  //fills in end screen
- $text = document.getElementById('text')
+ $text = document.getElementById('text');
  $dlist = ["Extra-Easy",
   "Easy",
   "Normal",
   "Hard",
-  "Extra-Hard"]
- $temp = "Hey <u>%0</u>,<br/> It looks like you %6 question <u>%1</u> and got <u>%2</u> points!<br/><br/>And you have played <u>%3</u> time(s)!<br/><br/>Your target score is <u>%4</u> on <u>%5</u> difficulty!"
+  "Extra-Hard"];
+ $temp = "Hey <u>%0</u>,<br/> It looks like you %6 question <u>%1</u> and got <u>%2</u> points!<br/><br/>And you have played <u>%3</u> time(s)!<br/><br/>Your target score is <u>%4</u> on <u>%5</u> difficulty!";
 
- $text.innerHTML = $temp.replace('%0', u.name).replace('%1', $q).replace('%2', u.pts2).replace('%3', u.plays).replace('%4', sessionStorage.target).replace('%5', $dlist[$diff-1])
+ $text.innerHTML = $temp.replace('%0', u.name).replace('%1', $q).replace('%2', u.pts2).replace('%3', u.plays).replace('%4', sessionStorage.target).replace('%5', $dlist[$diff-1]);
 
  if (pass) {
-  $text.innerHTML = $text.innerHTML.replace('%6', "you completed your challenge at")
-  document.getElementById('end').innerHTML ="You Win!"
+  $text.innerHTML = $text.innerHTML.replace('%6', "you completed your challenge at");
+  document.getElementById('end').innerHTML ="You Win!";
  } else {
-  $text.innerHTML = $text.innerHTML.replace('%6', "losed but you did last until")
+  $text.innerHTML = $text.innerHTML.replace('%6', "you losed but you did last until");
  }
 }
 
 //game functions
 function start() {
- const form = document.getElementById("setup")
-var name = form.name.value
+var name = form.name.value;
 
  if (name != "" && /[A-Za-z][1-9][ ]/) {
   if (name.length < 30 && name.length > 1) {
-   sessionStorage.diff = form.diff.value
-   sessionStorage.target = form.target.value
-   u.name = name
+  	if(form.target.value != isNaN){
+   sessionStorage.diff = form.diff.value;
+   sessionStorage.target = form.target.value;
+   u.name = name;
 
    game();
-   pages('game')
+  	}
   } else {
-   alert("Keep name under 30 characters!"); name = ""
+   alert("Keep name under 30 characters!"); name = "";
   }
  } else {
-  alert("Please enter your name.\n\nName conditions:\n> Most include characters\n> Can include spaces")
+  alert("Please enter your name.\n\nName conditions:\n> Most include characters\n> Can include spaces");
  }
 }
 
 function game() {
+	pages('game');
  $q = 1; $pts = 0;
- document.getElementById("q").innerHTML = $q
- $diff = Number(sessionStorage["diff"])
- $base = $diff * 9
- $a = document.getElementById('a')
- new_sum()
+ document.getElementById("q").innerHTML = $q;
+ $diff = Number(sessionStorage.diff);
+ $base = $diff * 9;
+ $a = document.getElementById('a');
+ new_sum();
 }
 
 function type_num(num) {
  if (num == 10) {
-  $a.value = ""
+  $a.value = "";
  } else {
-  $a.value += "" + num
+  $a.value += "" + num;
  }
 }
 
 function new_sum() {
- $type = 0
+ $type = 0;
  /* random base gen */
  $num = [Math.floor(Math.random() * $base + 1),
-  Math.floor(Math.random() * $base + 1)]
+  Math.floor(Math.random() * $base + 1)];
 
  /* check difficulty level */
  if ($diff == 1) {
-  $type = 1
+  $type = 1;
  } else if ($diff == 2 || $diff == 3) {
-  $type = Math.floor(Math.random()*1 + 1)
+  $type = Math.floor(Math.random()*1 + 1);
  } else if ($diff == 4 || $diff == 5) {
-  $type = Math.floor(Math.random()*2 + 1)
+  $type = Math.floor(Math.random()*2 + 1);
  }
 
  /* generate sums */
  if ($type == 1) {
-  $answer = $num[0] + $num[1]
-  $sum = "+"
+  $answer = $num[0] + $num[1];
+  $sum = "+";
  } else if ($type == 2) {
   if ($num[1] > $num[0]) {
-   new_sum()
+   new_sum();
   }
-  $answer = $num[0] - $num[1]
-  $sum = "-"
+  $answer = $num[0] - $num[1];
+  $sum = "-";
  } else if ($type == 3) {
   $answer = $sum[0] * $num[1];
   $sum = "x";
@@ -132,22 +138,23 @@ function check() {
 
  if ($a.value != "") {
   if ($a.value == $answer) {
-   $q = $q + 1
-   $pts += $diff
+   $q = $q + 1;
+   $pts += $diff;
+   let target;
    
-   $target = Number(sessionStorage.target)
- if ($target != isNaN) {
-  if ($pts > $target-1) {
-   end_game($pts,true)
+   target = Number(sessionStorage.target);
+ if (target != isNaN) {
+  if ($pts > target-1) {
+   end_game($pts,true);
   }
- }
+ } else {target = "Any"}
    
-   document.getElementById("q").innerHTML = $q
-   document.getElementById("pts").innerHTML = $pts
-   $a.value = ""
-   new_sum()
+   document.getElementById("q").innerHTML = $q;
+   document.getElementById("pts").innerHTML = $pts + " Points";
+   $a.value = "";
+   new_sum();
   } else if ($a.value != "") {
-   end_game($pts,false)
+   end_game($pts,false);
   }
  }
 }
@@ -156,9 +163,9 @@ function check() {
 function pages(pg) {
  if (page[pg].style.display != 'block') {
   for (var x = 0; x < page.length; x++) {
-   page[x].style.display = 'none'
+   page[x].style.display = 'none';
 
   }
-  page[pg].style.display = 'block'
+  page[pg].style.display = 'block';
  }
 }
