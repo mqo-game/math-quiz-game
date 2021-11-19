c81f6dd772b3bf215555828244cd84079fdf0cb0
McJoe21 <39857764+McJoe21@users.noreply.github.com>
2021-11-19 21:48:41 +0000

    Update 0.5.0 [BETA 4] Merge + Fixes

--- source/app.js
+++ source/app.js
@@ -1,4 +1,4 @@
-let toCache = ["style.css", "js/jquery.js", "js/script.js", "index.html","feedback.html", "images/clouds.png", "images/night.jpg", "images/logo.png", "icons/32x32.png", "icons/192x192.png", "icons/512x512.png", "icons/favicon.ico", "https://fonts.googleapis.com/css2?family=Ranchers&display=swap"], cacheName = "mqo-game-v0.5.0-b3"; 
+let toCache = ["style.css", "js/jquery.js", "js/script.js", "index.html","feedback.html", "images/clouds.png", "images/night.jpg", "images/logo.png", "icons/32x32.png", "icons/192x192.png", "icons/512x512.png", "icons/favicon.ico", "https://fonts.googleapis.com/css2?family=Ranchers&display=swap"], cacheName = "mqo-game-v0.5.0-b5"; 
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -14,7 +14,7 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((resp) => {
       return resp || fetch(event.request).then((response) => {
-        return caches.open('v1').then((cache) => {
+        return caches.open(cacheName).then((cache) => {
           cache.put(event.request, response.clone());
           return response;
         });
